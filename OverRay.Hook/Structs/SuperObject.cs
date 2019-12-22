581a98c7edeb073d4dbdf60b3c5e1b867e0207f8
Robin Sonneveld <r.t.sonneveld@gmail.com>
2019-12-22 01:42:57 +0100

    Start on target visualization

---
+++ OverRay.Hook/Structs/SuperObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OverRay.Hook.Structs
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SuperObject
+    {
+        public int type;
+        public IntPtr engineObjectPtr;
+    }
+}
