c3d649675804aea6d8f6130d2a8e091efa778234
Spitfire_x86 <pingwin44@gmail.com>
2019-12-23 02:10:26 +0100

    Merge branch 'textures'

--- OverRay.Hook/Detour.cs
+++ OverRay.Hook/Detour.cs
@@ -43,5 +43,33 @@ namespace OverRay.Hook
         {
             Interface.GameClosed();
         }
+
+        public void GetTextures()
+        {
+            IntPtr tPtr = (IntPtr)0x502680;
+            IntPtr tMemChannelsPtr = (IntPtr)0x501660;
+
+
+            uint[] tMemChannels = new uint[1024];
+            for (int i = 0; i < tMemChannels.Length; i++)
+            {
+                tMemChannels[i] = (uint) Marshal.ReadInt32(tMemChannelsPtr + 4 * i);
+            }
+
+            List<Texture> textures = new List<Texture>();
+            for (int i = 0; i < tMemChannels.Length; i++)
+            {
+                IntPtr textureStructPtr = Marshal.ReadIntPtr(tPtr + 4 * i);
+                if (textureStructPtr != IntPtr.Zero && tMemChannels[i] != 0xC0DE0005)
+                {
+                    Texture texture = new Texture(textureStructPtr);
+                    Game.textures.Add(texture);
+                    if (texture.Name == @"textures_personnages\divers\gra03_lum_my_ad.tga")
+                    {
+                        Interface.WriteLog(Convert.ToString((int)texture.Pointer, 16));
+                    }
+                }
+            }
+        }
     }
 }
