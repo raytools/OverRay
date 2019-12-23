b526cfe3889f7024ad99e9a21630850f1e5e4e80
Spitfire_x86 <pingwin44@gmail.com>
2019-12-23 02:10:26 +0100

    texture reading test

--- OverRay.Hook/Detour.cs
+++ OverRay.Hook/Detour.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading;
 using EasyHook;
 
@@ -14,6 +15,8 @@ namespace OverRay.Hook
             Interface = RemoteHooking.IpcConnectClient<RemoteInterface>(channelName);
             Interface.IsInstalled(RemoteHooking.GetCurrentProcessId());
             Game = new GameFunctions(Interface);
+
+            GetTextures();
         }
 
         private RemoteInterface Interface { get; }
@@ -34,6 +37,7 @@ namespace OverRay.Hook
 
                 RemoteHooking.WakeUpProcess();
 
+
             }
             catch (Exception e)
             {
@@ -42,6 +46,34 @@ namespace OverRay.Hook
 
             while (true) Thread.Sleep(100);
 
+        }
+
+        public void GetTextures()
+        {
+            IntPtr tPtr = (IntPtr)0x502680;
+            IntPtr tMemChannelsPtr = (IntPtr)0x501660;
+
+
+            uint[] tMemChannels = new uint[1024];
+            for (int i = 0; i < tMemChannels.Length; i++)
+            {
+                tMemChannels[i] = (uint) Marshal.ReadInt32(tMemChannelsPtr + 4 * i);
+            }
+
+            List<Texture> textures = new List<Texture>();
+            for (int i = 0; i < tMemChannels.Length; i++)
+            {
+                IntPtr textureStructPtr = Marshal.ReadIntPtr(tPtr + 4 * i);
+                if (textureStructPtr != IntPtr.Zero && tMemChannels[i] != 0xC0DE0005)
+                {
+                    Texture texture = new Texture(textureStructPtr);
+                    Game.textures.Add(texture);
+                    if (texture.Name == @"textures_personnages\divers\gra03_lum_my_ad.tga")
+                    {
+                        Interface.WriteLog(Convert.ToString((int)texture.Pointer, 16));
+                    }
+                }
+            }
         }
     }
 }
--- OverRay.Hook/GameFunctions/VEngine.cs
+++ OverRay.Hook/GameFunctions/VEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using OverRay.Hook.Structs;
 using OverRay.Hook.Utils;
@@ -12,6 +13,11 @@ namespace OverRay.Hook
 
         internal static readonly IntPtr VEnginePtr = (IntPtr)0x40ADA0;
         internal readonly VEngine OVEngine = Marshal.GetDelegateForFunctionPointer<VEngine>(VEnginePtr);
+
+        public List<Texture> textures = new List<Texture>();
+
+        private int test1;
+        private int frameCounter;
 
         internal byte HVEngine()
         {
@@ -40,6 +46,37 @@ namespace OverRay.Hook
                 //{
                 //    OVAddParticle(125, pos1, pos2, rayIcon, 5);
                 //}
+
+                
+                if (test1 + 1 < textures.Count)
+                {
+                    if (frameCounter > 3)
+                    {
+                        frameCounter = 0;
+                        test1++;
+                    }
+
+                    int pointer = (int) textures[test1].Pointer;
+
+                    if (pointer > 0x00500000 && pointer < 0x20000000)
+                    {
+                        Vector3 rpos1 = new Vector3 { X=10, Y=10, Z=0 };
+                        Vector3 rpos2 = new Vector3 { X=40, Y=40, Z=255 };
+                        using (StructPtr pos1 = new StructPtr(rpos1), pos2 = new StructPtr(rpos2))
+                        {
+                            OVAddParticle(125, pos1, pos2, (int)textures[test1].Pointer, 11);
+                            //Interface.WriteLog(textures[test1].Name);
+                            //Interface.WriteLog("0x"+Convert.ToString(pointer, 16));
+                        }
+                    }
+                    else
+                    {
+                        Interface.WriteLog($"Skipping 0x{Convert.ToString(pointer, 16)}: {textures[test1].Name}");
+                    }
+                    
+                    frameCounter++;
+                }
+                
             }
             catch (Exception e)
             {
